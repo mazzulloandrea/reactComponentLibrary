a7aa8f9b5b80b5076d55ab469d057a5b07c9c401
Andrea Mazzullo <mazzulloandrea@gmail.com>
2023-11-28 12:41:09 +0100

    update version to 0.0.2

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
-const Button = (props) => {
-	const { label = "" } = props;
-	return <button>{label}</button>;
+const Button = props => {
+  const { label = '' } = props;
+  return <button style="rounded-md">{label}</button>;
 };
 
 export default Button;
